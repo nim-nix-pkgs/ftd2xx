738579ff865008bb30ef74499a6231dda5b2c905
hugosenari <hugosenari@gmail.com>
2022-03-27 18:16:21 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-ftd2xx-main.ref   = "refs/heads/main";
   inputs.src-ftd2xx-main.owner = "leeooox";
   inputs.src-ftd2xx-main.repo  = "ftd2xx";
+  inputs.src-ftd2xx-main.dir   = "";
   inputs.src-ftd2xx-main.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
